ad189900a676a395ec378915a555fdb8c0c07a59
Tom <tbvanreijn@gmail.com>
2024-06-24 20:19:43 +0200

    test

--- app/overview/layout.tsx
+++ app/overview/layout.tsx
@@ -20,10 +20,5 @@ export default async function RootLayout({
     return <Login />;
   }
 
-  return (
-    <div className="flex w-full flex-col px-4 lg:px-40">
-      {children}
-      <Guia />
-    </div>
-  );
+  return <div className="flex w-full flex-col px-4 lg:px-40">{children}</div>;
 }
